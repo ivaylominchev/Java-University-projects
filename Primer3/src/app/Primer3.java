e4dd674104f8bb8873e3a5b970dfcf2ccb2f79b5
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-10-30 12:59:44 +0200

    imports

--- Primer3/src/app/Primer3.java
+++ Primer3/src/app/Primer3.java
@@ -4,10 +4,9 @@
  */
 package app;
 
-/**
- *
- * @author Acer
- */
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 public class Primer3 extends javax.swing.JFrame {
 
     /**
@@ -40,6 +39,11 @@ public class Primer3 extends javax.swing.JFrame {
         jLabel1.setText("jLabel1");
 
         jButton1.setText("Задача1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -88,6 +92,10 @@ public class Primer3 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
