b3f47d7117567767f4029fb083e5a376641108ae
Ivaylo Minchev <159180386+ivaylominchev@users.noreply.github.com>
2024-10-30 11:26:44 +0200

    change

--- Primer2/src/app/Primer2.java
+++ Primer2/src/app/Primer2.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author Acer
- */
+import java.util.*;
+import javax.swing.*;
+import java.awt.*;
 public class Primer2 extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Primer2
-     */
+    
     public Primer2() {
         initComponents();
     }
@@ -34,6 +28,11 @@ public class Primer2 extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("Задача1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -74,6 +73,10 @@ public class Primer2 extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
